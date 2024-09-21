33d9880d541af9fb569b4dddae24cc64c64f5d4b
Reece Thandi <reecethandi11@gmail.com>
2024-09-21 23:12:27 +0100

    Added links on work history timeline cards

--- src/app/aboutpage/aboutpage.component.ts
+++ src/app/aboutpage/aboutpage.component.ts
@@ -1,10 +1,6 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Apollo } from "apollo-angular";
-import { Observable, of } from 'rxjs';
-import { GET_WORKINFORMATION } from '../graphql.operations' 
 import { CommonModule } from '@angular/common';
-import { getDateFormat } from '../../utilities';
 import { WorkhistoryComponent } from "../workhistory/workhistory.component";
 
 @Component({
@@ -16,29 +12,4 @@ import { WorkhistoryComponent } from "../workhistory/workhistory.component";
 })
 
 export class AboutpageComponent {
-  workInformation$: Observable<Array<any>> = of([]);
-  error: any;
-  cursor: HTMLElement;
-
-  constructor(private apollo: Apollo) { }
-
-  ngOnInit(): void {
-    this.apollo.watchQuery({
-      query: GET_WORKINFORMATION
-    }).valueChanges.subscribe(({ data, error }: any) => {
-
-      // Prevent readonly assignment error
-      const localData = JSON.parse(JSON.stringify(data));
-
-      // Split technologyUsed string into an array of technologies for each job
-      for(let i = 0; i < localData?.workcards.length; i++) {
-        localData.workcards[i].startDate = getDateFormat(localData?.workcards[i].startDate)
-        localData.workcards[i].endDate = getDateFormat(localData?.workcards[i].endDate)
-        localData.workcards[i].technologyUsed = localData?.workcards[i].technologyUsed.split(",")
-      }
-
-      this.workInformation$ = of(localData?.workcards);
-      this.error = error
-    });
-  }
 }
