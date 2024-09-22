a1ae5bfaf3456d13fb5f1f067ea15dbd4f93de89
Reece Thandi <reecethandi11@gmail.com>
2024-09-22 19:22:04 +0100

    Removed unused imports in TypeScript files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgClass } from '@angular/common';
 import { RouterLinkActive } from '@angular/router';
