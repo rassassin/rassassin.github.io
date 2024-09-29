3122208597e7ebc6d99e9f8a613fd8d7a2eae51b
Reece Thandi <reecethandi11@gmail.com>
2024-09-29 15:52:25 +0100

    linting changes

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,13 +1,26 @@
 export function getRandomFloat(max: number = 0.3, min: number = 0): number {
-     return min + Math.random() * (max - min + 1) 
+  return min + Math.random() * (max - min + 1);
 }
 
-const monthStrings = ["Present", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
+const monthStrings = [
+  'Present',
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
 
 export function getDateFormat(date: string) {
-     const dateArr = date.split("-")
-     const monthString = monthStrings[parseInt(dateArr[1])]
-     const yearString = dateArr[0]
-     return `${monthString} ${yearString}`;
+  const dateArr = date.split('-');
+  const monthString = monthStrings[parseInt(dateArr[1])];
+  const yearString = dateArr[0];
+  return `${monthString} ${yearString}`;
 }
