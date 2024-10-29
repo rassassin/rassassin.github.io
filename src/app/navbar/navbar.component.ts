4d464231634f1237948eab9d31d71f46bee00608
Reece Thandi <reecethandi11@gmail.com>
2024-10-29 20:29:34 +0000

    updated navbar to be responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, NgClass, RouterLinkActive],
+  imports: [RouterModule, NgClass, RouterLinkActive, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
+@HostListener('document:click', ['$event'])
 export class NavbarComponent {
+  isMenuOpened: boolean = false;
   constructor() {}
+
+  toggleMenu(): void {
+    this.isMenuOpened = !this.isMenuOpened;
+  }
 }
